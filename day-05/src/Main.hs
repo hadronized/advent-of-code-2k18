ec6b57fc6b0a8b6a1c63e228484060f24c341dbc
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2018-12-07 01:03:06 +0100

    Day 5, part 2.

--- day-05/src/Main.hs
+++ day-05/src/Main.hs
@@ -3,7 +3,12 @@ module Main where
 import Data.Char (isLetter, isLower, isUpper, toLower)
 
 main :: IO ()
-main = getContents >>= print . length . reduce
+main = do
+  input <- getContents
+  print . length $ reduce input
+  print (bruteForce input)
+
+-- part 1
 
 reduce :: String -> String
 reduce = go []
@@ -14,3 +19,11 @@ reduce = go []
       | not (isLetter b) = go (a:xs) bs
       | (toLower a /= toLower b) || (isLower a && isLower b) || (isUpper a && isUpper b) = go (b:a:xs) bs
       | otherwise = go xs bs
+
+-- part 2
+
+bruteForce :: String -> Int
+bruteForce polymers = minimum (map length allReduced)
+  where
+    types = ['a' .. 'z']
+    allReduced = map (\c -> reduce $ filter (\x -> toLower x /= c) polymers) types

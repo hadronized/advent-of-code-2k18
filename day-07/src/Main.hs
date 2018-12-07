6c28caa67b2673cc3b927da257195d681ffff53c
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2018-12-07 22:07:25 +0100

    Should be good. :(

--- day-07/src/Main.hs
+++ day-07/src/Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
+import Data.Char (ord)
 import Data.Foldable (foldr')
-import Data.List (sort)
+import Data.List (sort, sortBy)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
+import Data.Ord (comparing)
 import Data.Set (Set)
 import qualified Data.Set as S
+import Numeric.Natural (Natural)
 
 main :: IO ()
 main = do
@@ -17,7 +20,10 @@ main = do
     Right steps' -> do
       let gr = foldr' accumDep mempty steps'
           plan = stepAvailable gr
+          planPar = stepAvailablePar 0 mempty gr
+
       print plan
+      print planPar
 
 data Dep = Dep {
     -- | Required step to go through first.
@@ -49,6 +55,33 @@ getAvailable gr = [step | (step, set) <- M.toList gr, S.null set]
 stepAvailable :: Graph -> [Step]
 stepAvailable gr = case sort (getAvailable gr) of
     [] -> []
-    (s:sx) -> s : stepAvailable (purgeDep s $ M.delete s gr)
+    (s:sx) -> s : stepAvailable (removeStep s gr)
+
+removeStep :: Step -> Graph -> Graph
+removeStep s = purgeDep s . M.delete s
   where
     purgeDep = fmap . S.delete
+
+-- part 2
+
+type Time = Natural
+type RemainingTime = Time
+type Work = Map Step RemainingTime
+
+maxWorkerNb :: Int
+maxWorkerNb = 5
+
+stepAvailablePar :: Time -> Work -> Graph -> Time
+stepAvailablePar currentTime work gr =
+    case M.lookupMin nextWork of
+      Just (_, timeIncrement) ->
+        stepAvailablePar (currentTime + timeIncrement) (fmap (decrWork timeIncrement) nextWork) gr'
+
+      Nothing -> currentTime
+  where
+    (done, running) = M.partition (== 0) work
+    gr' = foldl (flip removeStep) gr (map fst $ M.toList done)
+    backlog = running <> (M.fromList . map timeStep $ getAvailable gr')
+    timeStep s = (s, fromIntegral $ 61 + ord s - ord 'A')
+    nextWork = M.take maxWorkerNb backlog
+    decrWork = flip (-)

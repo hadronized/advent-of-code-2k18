43c61018d42fb2a2c4cd9150fce8c61d4fc62d81
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2018-12-15 01:23:36 +0100

    Better day-12.

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,8 +1,8 @@
+use std::collections::VecDeque;
 use std::io::{Read, stdin};
-use std::iter::repeat;
 use std::mem;
 
-const PADDING: usize = 20;
+const GENERATIONS: usize = 20;
 
 fn main() {
   let mut input = String::new();
@@ -13,7 +13,65 @@ fn main() {
   let mut pots2 = pots.clone();
   let rules = parsed.rules;
 
-  for _ in 0 .. 20 {
+  for _ in 0 .. GENERATIONS {
+    // account for left and right paddings
+
+    for pot in &pots {
+      print!("{}", if pot.1 == Pot::Plant { '#' } else { '.' });
+    }
+
+    println!("");
+
+    if pots[0].1 == Pot::Plant {
+      for _ in 0 .. 5 {
+        pots.push_front((pots[0].0 - 1, Pot::Empty));
+        pots2.push_front((pots2[0].0 - 1, Pot::Empty));
+      }
+    } else if pots[1].1 == Pot::Plant {
+      for _ in 0 .. 4 {
+        pots.push_front((pots[0].0 - 1, Pot::Empty));
+        pots2.push_front((pots2[0].0 - 1, Pot::Empty));
+      }
+    } else if pots[2].1 == Pot::Plant {
+      for _ in 0 .. 3 {
+        pots.push_front((pots[0].0 - 1, Pot::Empty));
+        pots2.push_front((pots2[0].0 - 1, Pot::Empty));
+      }
+    } else if pots[3].1 == Pot::Plant {
+      for _ in 0 .. 2 {
+        pots.push_front((pots[0].0 - 1, Pot::Empty));
+        pots2.push_front((pots2[0].0 - 1, Pot::Empty));
+      }
+    } else if pots[4].1 == Pot::Plant {
+      pots.push_front((pots[0].0 - 1, Pot::Empty));
+      pots2.push_front((pots2[0].0 - 1, Pot::Empty));
+    }
+
+    if pots[pots.len() - 1].1 == Pot::Plant {
+      for _ in 0 .. 5 {
+        pots.push_back((pots[pots.len() - 1].0 + 1, Pot::Empty));
+        pots2.push_back((pots2[pots2.len() - 1].0 + 1, Pot::Empty));
+      }
+    } else if pots[pots.len() - 2].1 == Pot::Plant {
+      for _ in 0 .. 4 {
+        pots.push_back((pots[pots.len() - 1].0 + 1, Pot::Empty));
+        pots2.push_back((pots2[pots2.len() - 1].0 + 1, Pot::Empty));
+      }
+    } else if pots[pots.len() - 3].1 == Pot::Plant {
+      for _ in 0 .. 3 {
+        pots.push_back((pots[pots.len() - 1].0 + 1, Pot::Empty));
+        pots2.push_back((pots2[pots2.len() - 1].0 + 1, Pot::Empty));
+      }
+    } else if pots[pots.len() - 4].1 == Pot::Plant {
+      for _ in 0 .. 2 {
+        pots.push_back((pots[pots.len() - 1].0 + 1, Pot::Empty));
+        pots2.push_back((pots2[pots2.len() - 1].0 + 1, Pot::Empty));
+      }
+    } else if pots[pots.len() - 5].1 == Pot::Plant {
+      pots.push_back((pots[pots.len() - 1].0 + 1, Pot::Empty));
+      pots2.push_back((pots2[pots2.len() - 1].0 + 1, Pot::Empty));
+    }
+
     for i in 2 .. pots.len() - 2 {
       let l2 = pots[i-2];
       let l1 = pots[i-1];
@@ -24,26 +82,24 @@ fn main() {
       let mut found = false;
 
       for rule in &rules {
-        if let Some(new_pot) = Pot::apply_rule(l2, l1, c, r1, r2, &rule.0, rule.1) {
-          pots2[i] = new_pot;
+        if let Some(new_pot) = Pot::apply_rule(l2.1, l1.1, c.1, r1.1, r2.1, &rule.0, rule.1) {
+          pots2[i].1 = new_pot;
           found = true;
           break;
         }
       }
 
       if !found {
-        pots2[i] = Pot::Empty;
+        pots2[i].1 = Pot::Empty;
       }
     }
 
     mem::swap(&mut pots, &mut pots2);
   }
 
-  let score: isize = pots.iter().enumerate().map(|(i, &x)| if x == Pot::Plant { i as isize - PADDING as isize } else { 0 }).sum();
+  let score: i64 = pots.iter().map(|x| if x.1 == Pot::Plant { x.0 } else { 0 }).sum();
   println!("score: {}", score);
 }
-
-// part 1
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum Pot {
@@ -79,15 +135,18 @@ impl Pot {
 
 #[derive(Debug)]
 struct Game {
-  initial_state: Vec<Pot>,
-  rules: Vec<([Pot; 5], Pot)>
+  initial_state: VecDeque<(i64, Pot)>,
+  rules: VecDeque<([Pot; 5], Pot)>,
 }
 
 impl Game {
   fn parse(s: &str) -> Self {
     let mut lines = s.lines();
-    let initial_state = lines.next().unwrap().chars().skip("initial state: ".len()).map(Pot::from_char);
-    let initial_state = repeat(Pot::Empty).take(PADDING).chain(initial_state).chain(repeat(Pot::Empty).take(PADDING)).collect();
+    let initial_state = lines
+      .next().unwrap()
+      .chars().skip("initial state: ".len())
+      .enumerate()
+      .map(|(i, c)| (i as i64, Pot::from_char(c))).collect();
 
     let _ = lines.next(); // remove the empty line
 

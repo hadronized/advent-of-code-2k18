ab96f5763a0769d9fc6beaef1495a79c91ce7f8a
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2018-12-24 17:55:15 +0100

    Meh meh meh.

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,6 +1,11 @@
 use std::collections::{HashSet, VecDeque};
 
+const MAX_HP: u8 = 200;
 const ATTACK_DMG: u8 = 3;
+
+fn main() {
+  println!("Hello, world!");
+}
 
 /// A block of a map.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
@@ -43,6 +48,53 @@ struct Map {
 }
 
 impl Map {
+  /// Parse the map out of a string.
+  fn parse(input: &str) -> Self {
+    let mut width = 0;
+    let mut height = 0;
+    let mut blocks = Vec::new();
+    let mut units = Vec::new();
+
+    for line in input.lines() {
+      if width == 0 {
+        width = line.len();
+      }
+
+      for (col, c) in line.chars().enumerate() {
+        match c {
+          '#' => blocks.push(Block::Wall),
+
+          '.' => blocks.push(Block::Empty),
+
+          'E' => {
+            let unit_id = units.len();
+            let pos = (col, height);
+            units.push((Unit { hp: MAX_HP, species: Species::Elf }, pos));
+            blocks.push(Block::Elf(unit_id));
+          }
+
+          'G' => {
+            let unit_id = units.len();
+            let pos = (col, height);
+            units.push((Unit { hp: MAX_HP, species: Species::Goblin }, pos));
+            blocks.push(Block::Goblin(unit_id));
+          }
+
+          _ => panic!("impossible map: {}", c)
+        }
+      }
+
+      height += 1;
+    }
+
+    Map {
+      width,
+      height,
+      blocks,
+      units
+    }
+  }
+
   /// Tell whether the game is over. If so, return the species that won along with the sum of the
   /// remaining units’ HP.
   fn is_game_over(&self) -> Option<(Species, u32)> {
@@ -72,7 +124,7 @@ impl Map {
   }
 
   /// Get all the possible positions a unit can move to.
-  fn get_available_destinations(&self, pos: &Pos) -> Vec<Pos> {
+  fn targets(&self, pos: &Pos) -> Vec<Pos> {
     [
       self.north_of(pos).and_then(|p| if self.block_at(&p) == Some(&Block::Empty) { Some(p) } else { None }),
       self.east_of(pos).and_then(|p| if self.block_at(&p) == Some(&Block::Empty) { Some(p) } else { None }),
@@ -110,16 +162,22 @@ impl Map {
 
   /// Get all the in-range target blocks of a given unit. These are all available destinations of
   /// all foes (even unreachable).
-  fn in_range_targets(&self, unit_id: UnitId) -> Vec<Pos> {
+  fn in_range_targets(&self, unit_id: UnitId) -> HashSet<Pos> {
     assert!(unit_id < self.units.len());
 
-    let mut tgt_list = Vec::new();
+    let mut tgt_list = HashSet::new();
     let &(ref current_unit, _) = &self.units[unit_id];
 
+    eprintln!("unit_id={:?}, species={:?}", unit_id, current_unit.species);
+
     for (i, (unit, pos)) in self.units.iter().enumerate() {
+      eprintln!("i={:?}, unit={:?}, pos={:?}", i, unit, pos);
       if i != unit_id {
+        eprintln!("oui id");
         if unit.species != current_unit.species {
-          tgt_list.extend(self.get_available_destinations(pos));
+          eprintln!("oui species");
+          eprintln!("should add: {:?}", self.targets(pos));
+          tgt_list.extend(self.targets(pos));
         }
       }
     }
@@ -129,12 +187,11 @@ impl Map {
 
   /// Prune a list of in-range target blocks by removing those who are not reachable (i.e. they
   /// would require to go through walls or other units.
-  fn prune_unreachable_targets(&self, unit_id: UnitId, targets: &[Pos]) -> HashSet<Pos> {
+  fn prune_unreachable_targets(&self, unit_id: UnitId, targets: &HashSet<Pos>) -> HashSet<Pos> {
     // algorithm: we explore around (without keeping trace of paths) until we cannot discover
     // anything; if we find a target block, we add it to the list of reachable
     assert!(unit_id < self.units.len());
 
-    let targets_set: HashSet<_> = targets.iter().cloned().collect();
     let mut reachables = HashSet::new();
     let mut visited = HashSet::new();
     let mut visiting = VecDeque::new();
@@ -146,12 +203,12 @@ impl Map {
       visited.insert(current);
 
       // check if this position is reachable; if so, add it to the reachables
-      if targets_set.contains(&current) {
+      if targets.contains(&current) {
         reachables.insert(current);
       }
 
       // get all the adjacent position we can go to 
-      let around = self.get_available_destinations(&current);
+      let around = self.targets(&current);
 
       // add them to the visiting only if we haven’t visited them already and that they’re don’t
       // contain a unit
@@ -164,6 +221,33 @@ impl Map {
 
     reachables
   }
+
+  // /// Get the nearest blocks out of a set.
+  // ///
+  // /// This function will find the path to the nearest block that can be found in the input set. If
+  // /// two paths are at the same distance, a rule of thumb is applied to return the one with the
+  // /// hightest probability.
+  // fn nearest_targets(&self, start: &Pos, targets: &HashSet<Pos>) -> PosPath {
+  //   let mut visiting = VecDeque::new();
+  //   let mut visited = HashSet::new();
+
+  //   // populate the list of visiting with the current adjacent blocks
+  //   for adj in self.targets(start) {
+  //     visiting.push_back(PosPath::new(adj));
+  //   }
+
+  //   while !visiting.is_empty() {
+  //     // get the last path and try to expand it
+  //     let path = visiting.pop_front().unwrap();
+  //     let recent = path.recent();
+
+
+  //     // if the current position is in the target list, it’s a solution path
+  //     if targets.contains(recent) {
+
+  //     }
+  //   }
+  // }
 
   fn north_of(&self, p: &Pos) -> Option<Pos> {
     if p.1 == 0 {
@@ -214,6 +298,68 @@ fn can_attack(foes: &[Option<UnitId>; 4]) -> bool {
   }
 }
 
-fn main() {
-  println!("Hello, world!");
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn block_at() {
+    let input = r#"
+#######
+#E..G.#
+#...#.#
+#.G.#G#
+#######"#;
+    let map = Map::parse(input);
+
+    assert_eq!(map.block_at(&(0, 0)), Some(&Block::Wall));
+    assert_eq!(map.block_at(&(1, 1)), Some(&Block::Elf(0)));
+    assert_eq!(map.block_at(&(4, 1)), Some(&Block::Goblin(1)));
+    assert_eq!(map.block_at(&(5, 3)), Some(&Block::Goblin(3)));
+  }
+
+  #[test]
+  fn targets() {
+    let input = r#"
+#######
+#E..G.#
+#...#.#
+#.G.#G#
+#######"#;
+    let map = Map::parse(input);
+
+    assert_eq!(map.targets(&(1, 1)).len(), 2);
+    assert_eq!(map.targets(&(4, 1)).len(), 2);
+    assert_eq!(map.targets(&(2, 3)).len(), 3);
+    assert_eq!(map.targets(&(5, 3)).len(), 1);
+  }
+
+  #[test]
+  fn in_range_targets() {
+    let input = r#"
+#######
+#E..G.#
+#...#.#
+#.G.#G#
+#######"#;
+    let map = Map::parse(&input[1..]);
+    let irt = map.in_range_targets(0);
+
+    assert_eq!(irt.len(), 6);
+  }
+
+  #[test]
+  fn prune_unreachable_targets() {
+    let input = r#"
+#######
+#E..G.#
+#...#.#
+#.G.#G#
+#######"#;
+    let map = Map::parse(&input[1..]);
+    let irt = map.in_range_targets(0);
+    let pruned = map.prune_unreachable_targets(0, &irt);
+
+    assert_eq!(pruned.len(), 4);
+  }
 }
